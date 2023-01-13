472c19d3b6e9da3e1efa6e1472bd457396bfbc6c
Shaun <seansama@pop-os.localdomain>
2023-01-13 14:13:02 +0300

    Lab complete

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About(props) {
+    return (
+        <div>
+            <aside>
+                <img src={props.image || "https://via.placeholder.com/215"}
+                     alt="blog logo"></img>
+                <p>{props.about}</p>
+            </aside>
+        </div>
+    )
+
+}
+export default About;
